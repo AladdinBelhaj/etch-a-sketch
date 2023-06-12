1bee810cde533ed41810de5517c9f3837ba30f92
AladdinBelhaj <alabelhaj2002@gmail.com>
2023-06-12 15:08:48 +0100

    Add range slider for canvas size

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-    let button = document.querySelector('button');
-    let container = document.getElementById('container');
+    var button = document.querySelector('button');
+    var container = document.getElementById('container');
+    var slider = document.getElementById('myRange');
+    var output = document.querySelector('span');
+
+    output.innerHTML = `${slider.value}x${slider.value}`; 
+    slider.oninput = function(){
+        output.innerHTML = `${this.value}x${this.value}`;
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+          }
+        for (let i = 0; i < this.value * this.value; i++) {
+            let div = document.createElement('div');
+            div.classList.add('grid');
+            div.style.flexBasis = `calc(100% / ${this.value})`;
+            container.appendChild(div);
+            let divs = document.querySelectorAll('.grid');
+            divs.forEach(div=>{
+                div.addEventListener('mouseover', function(){
+                    div.style.backgroundColor = "red";
+                });
+            })
+     }
+    }
 
         for (let i = 0; i < 16 * 16; i++) {
             let div = document.createElement('div');
@@ -13,30 +35,6 @@
                 });
             })
         }
-
-
-        button.addEventListener('click', function() {
-            while (container.firstChild) {
-                container.removeChild(container.firstChild);
-              }
-
-            n = parseInt(prompt('Give value', '0'));
-            for (let i = 0; i < n * n; i++) {
-                let div = document.createElement('div');
-                div.classList.add('grid');
-                div.style.flexBasis = `calc(100% / ${n})`;
-                container.appendChild(div);
-                let divs = document.querySelectorAll('.grid');
-                divs.forEach(div=>{
-                    div.addEventListener('mouseover', function(){
-                        div.style.backgroundColor = "red";
-                    });
-                })
-         }
-     });
-
-
-
 
 
 
